9b859076ff33a89d9bb657544605382b2f21e31a
John Bohn <jjbohn@gmail.com>
2016-04-11 09:59:24 -0400

    Add minimal authentication route

--- src/jsx/app/routes.jsx
+++ src/jsx/app/routes.jsx
@@ -1,13 +1,15 @@
-import { Route, Router } from 'react-router';
-import { createHistory } from 'history';
+import { Route, Router } from "react-router";
+import { createHistory } from "history";
 
-import Blank from './routes/blank';
+import Blank from "./routes/blank";
+import SignUp from "./routes/sign_up";
 
 export default (withHistory, onUpdate) => {
   const history = createHistory();
   return (
     <Router history={history} onUpdate={onUpdate}>
-      <Route path='/' component={Blank} />
+      <Route path="/" component={Blank} />
+      <Route path="/sign_up" component={SignUp} />
     </Router>
   );
 };
